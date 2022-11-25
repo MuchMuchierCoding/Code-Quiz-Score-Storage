95883b4d00c54404e0143b0faa558e6a8f22006e
Five Pandas <amandapaul1223@gmail.com>
2022-11-25 16:08:21 -0700

    getting buttons to work

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,11 @@
-
 // Declared variables
 var highScore = document.querySelector("#highScore");
-var clear = document.querySelector("#clear");
-var goBack = document.querySelector("#goBack");
-var startButton =document.querySelector("#startQuiz");
+/*var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");*/
+const startBtn = document.getElementById("#startQuiz");
 var timerCountDown = document.querySelector("#timer");
 
-
-//Event listener to start quiz and timer 
-function startTimer() {
-    timer = setInterval(function() {
-        timerCountDown--;
-        startButton.textContent = timerCountDown;
-        if (quizComplete) {
-            clearInterval(timer);
-        }
-    })
-}
-
-/*startButton.addEventListener("click", function () {
-    countDown () {
-        var timeLeft = 10;
-    }
-    var timeIntervel = setInterval(function () {
-        if (timeLeft > 10) {
-            timerCountDown = timeLeft;
-            timeLeft--;
-        } else if (timeLeft === 1) {
-            timerCountDown = timeLeft;
-            timeLeft--; 
-        } else {
-            timerCountDown = '';
-            clearInterval(timeIntervel);
-            displayMessage("Time's Up!");
-        }
-   }, 1000); 
-}
-
-*/
-
-// Event listener to clear scores 
-clear.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload();
-});
-// Retreives local stroage 
-var allScores = localStorage.getItem("allScores");
-allScores = JSON.parse(allScores);
-
-if (allScores !== null) {
-
-    for (var i = 0; i < allScores.length; i++) {
-
-        var createLi = document.createElement("li");
-        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
-        highScore.appendChild(createLi);
-
-    }
-}
-// Event listener to move to index page
-goBack.addEventListener("click", function () {
-    window.location.replace("./index.html");
-});
+console.log(startBtn);
 
 
 
@@ -78,5 +22,3 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and my score
 */
-
-
