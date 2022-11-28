4ca211b3b48d93239bd2d8655ce2bf8adfdb7227
Five Pandas <amandapaul1223@gmail.com>
2022-11-27 19:18:27 -0700

    Need to get questions to populate and accuracy announcement

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,120 @@
-// Declared variables
-var highScore = document.querySelector("#highScore");
-/*var clear = document.querySelector("#clear");
-var goBack = document.querySelector("#goBack");*/
-var startBtn = document.getElementById('startQuiz');
-var questionContainerEl = document.getElementById('questionContainer');
-var answerOptions = document.getElementById('answers');
-var questionBank = document.getElementById('question');
-var timerCountDown = document.querySelector("#timer");
-var title = document.getElementById('quizTitle');
-var instructions = document.getElementById('quizInstructions'); 
+var startBtn = document.getElementById('startQuiz')
+/*var nextButton = document.getElementById('next-btn')*/
+var questionContainerEl = document.getElementById('questionContainer')
+var displayedQuestion = document.getElementById('question')
+var answerOptions = document.getElementById('answers')
+var instructions = document.getElementById('quizInstructions')
+var title = document.getElementById('quizTitle')
 var questionOptions, questionBank
-var questionElement = document.getElementById('question');
-var answerButtonsElements = document.getElementById('answers');
 
-startBtn.addEventListener('click', startGame);
+startBtn.addEventListener('click', startGame)
 
+/*nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})*/
+
+//Initiates game at start button click. hides title and quiz instructions
 function startGame() {
-    console.log('Started')
-    startBtn.classList.add('hide')
-    instructions.classList.add('hide')
-    questionContainerEl.classList.remove('hide')
-    setNextQuestion()
-    title.classList.add('hide')
-    instructions.classList.add('hide')
-    questionOptions = questions.sort(() => Math.random() - .5)
-    questionBank = 0
-    questionSets()
-};
+  startBtn.classList.add('hide')
+  instructions.classList.add('hide')
+  title.classList.add('hide')
+  questionOptions = questions.sort(() => Math.random() - .5)
+  questionBank = 0
+  questionContainerEl.classList.remove('hide')
+  setNextQuestion()
+}
+
 
 function setNextQuestion() {
-    showQuestion(questionOptions[questionBank])
+  resetState()
+  showQuestion(questionOptions[questionBank])
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
-}
-
-function selectAnswer() {
-
-}
-
-function questionSets() {
-    showQuestion()
-}
-
-
-
-var question = [
-    {
-       question: 'What is 2+2?',
-       answers: [
-        {text: '4', correct: true},
-        {text: '22', correct: false}
-       ] 
+  displayedQuestion.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
     }
+    button.addEventListener('click', selectAnswer)
+    answerOptions.appendChild(button)
+  })
+}
+
+function resetState() {
+  setAccuracy(document.body)
+ /* nextButton.classList.add('hide')*/
+  while (answerOptions.firstChild) {
+    answerOptions.removeChild(answerOptions.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setAccuracy(document.body, correct)
+  Array.from(answerOptions.children).forEach(button => {
+    setAccuracy(button, button.dataset.correct)
+  })
+  if (questionOptions.length > questionBank + 1) {
+    /*nextButton.classList.remove('hide')*/
+  } else {
+    startBtn.innerText = 'Restart'
+    startBtn.classList.remove('hide')
+  }
+}
+
+//show whether question is correct or incorrect
+function setAccuracy(element, correct) {
+  setAccuracy(element)
+  if (correct) {
+    element.classList.add('correct')
+    return 'Correct!';
+  } else {
+    element.classList.add('wrong')
+    return 'Incorrect!';
+  }
+}
+
+function setAccuracy(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+// question bank 
+const questions = [
+  {
+    question: 'String values must be enclodes within ___ when being assigned to variables.',
+    answers: [
+      { text: '1. commas', correct: false },
+      { text: '2. quotes', correct: true },
+      { text: '3. curly brackets', correct: false},
+      { text: '4. parenthesis', correct: false},
+    ]
+  },
+
+  {
+    question: 'Arrays in JavaScript can be used to store ____.',
+    answers: [
+      { text: '1. numbers and strings', correct: false },
+      { text: '2. other arrays', correct: false },
+      { text: '3. booleans', correct: false},
+      { text: '4. all of the above', correct: true }
+    ]
+  },
+  {
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answers: [
+      { text: '1. JavaScript', correct: false },
+      { text: '2. terminal/bash', correct: false },
+      { text: '3. for loops', correct: false },
+      { text: '4. console.log', correct: true }
+    ]
+  },
 ]
 
 /*
